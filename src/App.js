3c359b15804f11b32811889a6f01543f385b6c2b
alkappe <alka.cappellazzo@gmail.com>
2021-01-25 20:37:21 +0100

    started doggy app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import dogplaceholder from './dogplaceholder.jpg';
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [image, updateImage] = useState(dogplaceholder);
+
+  const getDoggyImage = () => {
+    axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(function (response) {
+      // handle success
+      updateImage(response.data.message)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="doggy__card-container doggy__box">
+      
+      <div className="doggy__colored-wrapper">
+        <div className="doggy__image-wrapper">
+          <img className="doggy__image" src={image} alt=""/>
+        </div>
+        <button className="doggy__btn" onClick={getDoggyImage}>
+        <h1 className="doggy__dogemojii">🐶</h1>
+        <p className="doggy__generate">generate another dog</p>
+      </button>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
